18eb97ea16169d777915d37ea37dc2fad35df062
Stig Otnes Kolstad <stig.otnes.kolstad@nrk.no>
2022-06-22 16:11:14 +0200

    chore: update reuse script and license info

--- _tools/openssl/create_self_signed_cert.sh
+++ _tools/openssl/create_self_signed_cert.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+# SPDX-FileCopyrightText: 2022 NRK
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
 openssl req -x509 -newkey rsa:4096 -sha256 -utf8 -days 365 -nodes -config openssl.conf -keyout cert.key -out cert.crt
