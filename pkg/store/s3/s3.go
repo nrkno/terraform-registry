7eb067a8c6ae9355315ac276fc58993c8f833bd1
Daniel Otto <daniel.otto@public.com>
2023-08-10 15:02:06 +0200

    Refactoring GetModuleVersion and helpers

--- pkg/store/s3/s3.go
+++ pkg/store/s3/s3.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"fmt"
 	"path/filepath"
+	"strings"
 
-	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/nrkno/terraform-registry/pkg/core"
@@ -16,55 +16,46 @@ type S3Store struct {
 	client *s3.S3
 	cache  map[string][]*core.ModuleVersion
 
-	bucket  string
-	prefix  string
-	profile string
-	region  string
+	bucketName string
+	prefix     string
 
 	logger *zap.Logger
 }
 
-func NewS3Store(bucket string, prefix string, region string, profile string, logger *zap.Logger) (*S3Store, error) {
+func NewS3Store(bucketName string, prefix string, logger *zap.Logger) (*S3Store, error) {
 	store := &S3Store{}
 
-	if bucket == "" {
-		return store, fmt.Errorf("%s", "missing parameter: 'bucket'")
-	}
-	if profile == "" {
-		profile = "default"
-	}
 	if logger == nil {
 		logger = zap.NewNop()
 	}
 
-	sessOpts := session.Options{
-		Profile:                 profile,
-		SharedConfigState:       session.SharedConfigEnable,
-		AssumeRoleTokenProvider: stscreds.StdinTokenProvider,
-	}
-	sess, err := session.NewSessionWithOptions(sessOpts)
+	sess, err := session.NewSession()
 	if err != nil {
 		return store, fmt.Errorf("session creation failed: %s", err)
 	}
+	logger.Debug("session created successfully")
+
+	_, err = sess.Config.Credentials.Get()
+	if err != nil {
+		return store, fmt.Errorf("session credentials not found: %s", err)
+	}
+	logger.Debug("session credentials found")
 
 	store = &S3Store{
-		client:  s3.New(sess),
-		cache:   make(map[string][]*core.ModuleVersion),
-		bucket:  bucket,
-		prefix:  prefix,
-		region:  region,
-		profile: profile,
-		logger:  logger,
+		client:     s3.New(sess),
+		cache:      make(map[string][]*core.ModuleVersion),
+		bucketName: bucketName,
+		prefix:     prefix,
 	}
 
 	return store, nil
 }
 
 func (s *S3Store) ListModuleVersions(ctx context.Context, namespace, name, provider string) ([]*core.ModuleVersion, error) {
+	modulePath := filepath.Join(s.bucketName, namespace, name, provider)
 	versions := make([]*core.ModuleVersion, 0)
-	path := filepath.Join(s.bucket, namespace, name, provider)
 	input := &s3.ListObjectsV2Input{
-		Bucket: &path,
+		Bucket: &modulePath,
 		Prefix: &s.prefix,
 	}
 	output, err := s.client.ListObjectsV2(input)
@@ -72,11 +63,13 @@ func (s *S3Store) ListModuleVersions(ctx context.Context, namespace, name, provi
 		return versions, err
 	}
 	contents := output.Contents
+	region := *s.client.Config.Region
 	for _, v := range contents {
 		version := v.Key
+		moduleVersionPath := filepath.Join(modulePath, *version)
 		versions = append(versions, &core.ModuleVersion{
 			Version:   *version,
-			SourceURL: fmt.Sprintf("s3::https://s3-%s.amazonaws.com/%s/%s/%s/%s.zip", "", "", "", "", ""),
+			SourceURL: genSourceURL(region, moduleVersionPath),
 		})
 	}
 
@@ -84,8 +77,26 @@ func (s *S3Store) ListModuleVersions(ctx context.Context, namespace, name, provi
 }
 
 func (s *S3Store) GetModuleVersion(ctx context.Context, namespace, name, provider, version string) (*core.ModuleVersion, error) {
-	// Check cache. Then look up directly and update cache
-	return &core.ModuleVersion{}, nil
+	v := &core.ModuleVersion{}
+	moduleVersionPath := filepath.Join(s.bucketName, namespace, name, provider, version)
+	input := &s3.ListObjectsV2Input{
+		Bucket: &moduleVersionPath,
+		Prefix: &s.prefix,
+	}
+	output, err := s.client.ListObjectsV2(input)
+	if err != nil {
+		return v, err
+	}
+	contents := output.Contents
+	region := *s.client.Config.Region
+	if len(contents) == 0 {
+		return v, err
+	}
+	v = &core.ModuleVersion{
+		Version:   version,
+		SourceURL: genSourceURL(region, moduleVersionPath),
+	}
+	return v, nil
 }
 
 func (s *S3Store) PublishModuleVersion() {
@@ -99,4 +110,9 @@ func (s *S3Store) UnpublishModuleVersion() {
 func (s *S3Store) ReloadCache(ctx context.Context) error {
 	// Traverse all modules with directory structure
 	return nil
+}
+
+func genSourceURL(region string, path string) string {
+	path = strings.TrimPrefix(path, "/")
+	return fmt.Sprintf("s3::https://s3-%s.amazonaws.com/%s/module.zip", region, path)
 }
