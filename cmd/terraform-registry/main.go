ad85563cc3a7c35601c527abf3cbdbf46f216e07
Daniel Otto <daniel.otto@public.com>
2023-08-17 16:12:40 +0200

    Check AWS region is set pre registry init

--- cmd/terraform-registry/main.go
+++ cmd/terraform-registry/main.go
@@ -38,6 +38,7 @@ var (
 
 	awsAccessKeyID     string
 	awsSecretAccessKey string
+	awsRegion          string
 	awsS3Bucket        string
 
 	gitHubToken       string
@@ -118,6 +119,7 @@ func main() {
 
 	awsAccessKeyID = os.Getenv("AWS_ACCESS_KEY_ID")
 	awsSecretAccessKey = os.Getenv("AWS_SECRET_ACCESS_KEY")
+	awsRegion = os.Getenv("AWS_REGION")
 	awsS3Bucket = os.Getenv("AWS_S3_BUCKET")
 
 	reg := registry.NewRegistry(logger)
@@ -273,6 +275,9 @@ func s3Registry(reg *registry.Registry) {
 	}
 	if awsSecretAccessKey == "" {
 		logger.Fatal("missing environment variable 'AWS_SECRET_ACCESS_KEY'")
+	}
+	if awsRegion == "" {
+		logger.Fatal("missing environment variable 'AWS_REGION'")
 	}
 	if awsS3Bucket == "" {
 		logger.Fatal("missing environment variable 'AWS_S3_BUCKET_NAME'")
